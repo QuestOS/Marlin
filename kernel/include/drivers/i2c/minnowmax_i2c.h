8e9bdc9abab7b7d435c38d1e86d15cb653586c78
Tom Cheng <czq@bu.edu>
2016-04-19 15:05:43 -0400

    1. added mraa_i2c_read_word_data. 2. added ads1115 test. It gives the same result as running on Arduino, though neither result makes sense...

--- kernel/include/drivers/i2c/minnowmax_i2c.h
+++ kernel/include/drivers/i2c/minnowmax_i2c.h
@@ -1,9 +1,10 @@
 #include "kernel.h"
 
-u8 byt_i2c_read_byte_data(u8 command);
-s32 byt_i2c_write_byte(u8 value);
-s32 byt_i2c_write_word(u16 value);
-s32 byt_i2c_write_word_data(u8 cmd, u16 value);
-void byt_i2c_xfer_init(u32 slave_addr);
+int byt_i2c_read_byte_data(u8);
+int byt_i2c_read_word_data(u8);
 int byt_i2c_read_bytes_data(u8, u8 *, u32);
+s32 byt_i2c_write_byte(u8);
+s32 byt_i2c_write_word(u16);
+s32 byt_i2c_write_word_data(u8, u16);
+void byt_i2c_xfer_init(u32 slave_addr);
 
